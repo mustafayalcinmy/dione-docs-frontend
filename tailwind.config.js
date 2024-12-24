177675e826fe4a1db1252f71562d0ddc6ee8b1c9
Uygar Akat <uygarakat@outlook.com>
2024-12-25 01:15:39 +0300

    frontend demo for presentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{html,ts}" // Adjust this path based on your project structure
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gruvbox: {
+          bg: "#282828", // Dark background
+          fg: "#ebdbb4", // Light foreground
+          red: "#cc241d",
+          green: "#98971a",
+          yellow: "#d79921",
+          blue: "#458588",
+          purple: "#b16286",
+          aqua: "#689d6a",
+          orange: "#d65d0e",
+        },
+      },
+    },
   },
   plugins: [],
 };
