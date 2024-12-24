d81bf210766d21c3cdb1f926f5b6a3d9b7573976
Mustafa YALÇIN <210229038@uzem.education>
2024-12-24 23:40:18 +0300

    changed main page

--- dione-docs/src/app/app.routes.ts
+++ dione-docs/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './core/components/main-page/main-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: '**', redirectTo: '' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
